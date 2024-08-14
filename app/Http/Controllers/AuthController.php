ac4498111c58d79f4a6acc35be4a5c6af40610ab
Ramadhan Agies Ananda Wahyudi <agieswahyudi@gmail.com>
2024-08-15 00:18:21 +0700

    Apply sanctum in authentication, create login and logout function, modified create_users__table migration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -16,7 +19,7 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        return 'hello world';
+        return 'register';
     }
 
     /**
@@ -24,15 +27,68 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        //
+        $validated = $request->validate(
+            [
+                'email' => 'required|email|exists:users',
+                'password' => 'required|min:8'
+            ],
+            // [
+            //     '' => ''
+            // ]
+        );
+
+        if ($validated) {
+            try {
+                if (Auth::attempt($validated, $request->remember_me)) {
+                    $user = User::where('email', $request->email)->first();
+                    $token = $user->createToken($user->id);
+                    $user_data = [
+                        "name" => $user->name,
+                        "email" => $user->email,
+                        "role_id" => $user->role_id,
+                        "is_active" => $user->is_active,
+                        "token" => $token->plainTextToken,
+                    ];
+
+                    return response()->json([
+                        "result" => "Success",
+                        "message" => "Login attempt successfully",
+                        "user_data" => $user_data,
+                    ], 200);
+                } else {
+                    return response()->json([
+                        "result" => "Failed",
+                        "message" => "Email or Password mismatch"
+                    ], 404);
+                }
+            } catch (\Throwable $th) {
+                return response()->json([
+                    "result" => "Error",
+                    "message" => $th->getMessage()
+                ], 404);
+            }
+        }
     }
 
 
     /**
      * LOGOUT METHOD.
      */
-    public function logout(string $id)
+    public function logout(Request $request)
     {
-        //
+        $user = Auth::user();
+        if ($user) {
+            $request->user()->currentAccessToken()->delete();
+            Auth::guard('web')->logout();
+            return response()->json([
+                "result" => "success",
+                "message" => "You're Logged Out"
+            ], 200);
+        }
+
+        return response()->json([
+            "result" => "error",
+            "message" => "User not authenticated"
+        ], 401);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,31 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
 
 // AUTH ROUTE 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 // AUTH ROUTE 
+
+// USER ROUTE 
+// Route::post('/users', [UsersController::class, 'index'])->name('register');
+Route::resource('users', UsersController::class);
+// USER ROUTE 
+
+Route::get('/test_api', function () {
+    return "testapi";
+});
